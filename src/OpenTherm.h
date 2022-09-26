1edb997adaf35600e883be62bdf9ed4b4b3644f4
opbokke <26428586+opbokke@users.noreply.github.com>
2022-09-26 23:51:24 +0200

    Update OpenTherm.h

--- src/OpenTherm.h
+++ src/OpenTherm.h
@@ -115,8 +115,8 @@ class OpenTherm
 public:
 	OpenTherm(int inPin = 4, int outPin = 5, bool isSlave = false);
 	volatile OpenThermStatus status;
-	void begin(void(*handleInterruptCallback)(void));
-	//void begin(void(*handleInterruptCallback)(void), void(*processResponseCallback)(unsigned long, OpenThermResponseStatus));
+	//void begin(void(*handleInterruptCallback)(void));
+	void begin(void(*handleInterruptCallback)(void), void(*processResponseCallback)(unsigned long, OpenThermResponseStatus));
 	bool isReady();
 	unsigned long sendRequest(unsigned long request);
 	bool sendResponse(unsigned long request);
